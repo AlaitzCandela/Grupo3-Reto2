f390c8f6e452992b39c01dc9cb02efeeef38854b
Raul-Melgosa <raul.melgosa@ikasle.egibide.org>
2021-11-16 09:51:36 +0100

    plantillas

---
+++ servidor/web/views/partials/menu.php
@@ -0,0 +1,59 @@
+<nav>
+    <ul>
+        <li>
+            <a href="">
+                <span class="icon logo"><img src="../img/tomNookLogo.svg" alt="Logo"></span>
+                <span class="title logo">Tom&nbsp;Nook</span>
+            </a>
+        </li>
+
+        <li class="selected">
+            <a href="">
+                <span class="icon"><ion-icon name="home-outline"></ion-icon></span>
+                <span class="title">Centro&nbsp;de&nbsp;control</span>
+            </a>
+        </li>
+
+        <li>
+            <a href="">
+                <span class="icon"><ion-icon name="people-outline"></ion-icon></span>
+                <span class="title">Clientes</span>
+            </a>
+        </li>
+
+        <li>
+            <a href="">
+                <span class="icon"><ion-icon name="chatbubble-outline"></ion-icon></span>
+                <span class="title">Mensajes</span>
+            </a>
+        </li>
+
+        <li>
+            <a href="">
+                <span class="icon"><ion-icon name="help-outline"></ion-icon></span>
+                <span class="title">Ayuda</span>
+            </a>
+        </li>
+
+        <li>
+            <a href="">
+                <span class="icon"><ion-icon name="settings-outline"></ion-icon></span>
+                <span class="title">Ajustes</span>
+            </a>
+        </li>
+
+        <li>
+            <a href="">
+                <span class="icon"><ion-icon name="lock-closed-outline"></ion-icon></span>
+                <span class="title">Seguridad</span>
+            </a>
+        </li>
+
+        <li>
+            <a href="">
+                <span class="icon"><ion-icon name="log-out-outline"></ion-icon></span>
+                <span class="title">Cerrar&nbsp;sesi&oacute;n</span>
+            </a>
+        </li>
+    </ul>
+</nav>
---
+++ servidor/web/views/partials/topbar.php
@@ -0,0 +1,17 @@
+<div class="topbar">
+    <div class="toggle">
+        <ion-icon name="menu-outline"></ion-icon>
+    </div>
+    <div class="search">
+        <label>
+            <input type="text" name="busqueda" id="search" placeholder="Buscar">
+            <ion-icon name="search-outline"></ion-icon>
+        </label>
+        <div class="filtro">
+            <ion-icon name="options-outline"></ion-icon>
+        </div>
+    </div>
+    <div class="profile-pic">
+        <img src="<?=$profile_pic?>" alt="Foto de perfil">
+    </div>
+</div>
