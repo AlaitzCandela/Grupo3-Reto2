225b0bf9f500a417e47ab99ddaf4f36cdc16ff94
Raul-Melgosa <raul.melgosa@ikasle.egibide.org>
2021-11-18 09:32:02 +0100

    anuncios.view

---
+++ servidor/web/views/anuncios.view.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<?php
+    require "./partials/metas.php";
+?>
+    <title>Descubrir anuncios</title>
+    <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
+    <div class="contenedor">
+        <?php require "./partials/menu.php";?>
+        <main>
+            <?php 
+                $profile_pic="../img/default_user.png";
+                require "./partials/topbar.php";
+            ?>
+            
+            <section>
+                <div class="cuadricula">
+                    <div class="anuncio">
+                        <img src="https://via.placeholder.com/1500" alt="">
+                    </div>
+
+                    <div class="anuncio">
+                        <img src="https://via.placeholder.com/1500" alt="">
+                    </div>
+
+                    <div class="anuncio">
+                        <img src="https://via.placeholder.com/1500" alt="">
+                    </div>
+
+                    <div class="anuncio">
+                        <img src="https://via.placeholder.com/1500" alt="">
+                    </div>
+
+                    <div class="anuncio">
+                        <img src="https://via.placeholder.com/1500" alt="">
+                    </div>
+
+                    <div class="anuncio">
+                        <img src="https://via.placeholder.com/1500" alt="">
+                    </div>
+
+                    <div class="anuncio">
+                        <img src="https://via.placeholder.com/1500" alt="">
+                    </div>
+
+                    <div class="anuncio">
+                        <img src="https://via.placeholder.com/1500" alt="">
+                    </div>
+
+                    <div class="anuncio">
+                        <img src="https://via.placeholder.com/1500" alt="">
+                    </div>
+
+                    <div class="anuncio">
+                        <img src="https://via.placeholder.com/1500" alt="">
+                    </div>
+
+                    <div class="anuncio">
+                        <img src="https://via.placeholder.com/1500" alt="">
+                    </div>
+
+                    <div class="anuncio">
+                        <img src="https://via.placeholder.com/1500" alt="">
+                    </div>
+
+                    <div class="anuncio">
+                        <img src="https://via.placeholder.com/1500" alt="">
+                    </div>
+
+                    <div class="anuncio">
+                        <img src="https://via.placeholder.com/1500" alt="">
+                    </div>
+
+                    <div class="anuncio">
+                        <img src="https://via.placeholder.com/1500" alt="">
+                    </div>
+                </div>
+            </section>
+
+        </main>
+    </div>
+    <?php require "./partials/scripts.php";?>
+    
+</body>
+</html>
+
