aa6b370ccd7d47596460b850eeb21f8fb7d07253
Raul-Melgosa <raul.melgosa@ikasle.egibide.org>
2021-11-18 12:45:33 +0100

    Inicio formulario anuncios

--- servidor/web/javaScript/app.js
+++ servidor/web/javaScript/app.js
@@ -1,8 +1,25 @@
 let toggle = document.querySelector('.toggle');
 let nav = document.querySelector('nav');
 let main = document.querySelector('main');
+let topbar = document.querySelector('.topbar');
+let imgInp = document.querySelector('#img-input');
 
 toggle.onclick = function() {
     nav.classList.toggle('active');
     main.classList.toggle('active');
+    topbar.classList.toggle('active');
 }
+
+imgInp.onchange = evt => {
+    const [file] = imgInp.files
+    if (file) {
+      document.querySelector('#preview').src = URL.createObjectURL(file);
+    }
+}
+
+$("textarea").each(function () {
+    this.setAttribute("style", "height:" + (this.scrollHeight) + "px;overflow-y:hidden;");
+  }).on("input", function () {
+    this.style.height = "auto";
+    this.style.height = (this.scrollHeight) + "px";
+});
