f9c87d6e82b6bc47f21b171c2fe801992259ea18
Raul-Melgosa <raul.melgosa@ikasle.egibide.org>
2021-11-19 13:25:24 +0100

    Cambios varios

---
+++ servidor/web/views/detalle-anuncio.view.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<?php
+    require "./partials/metas.php";
+?>
+    <title>Plantilla bien perrona</title>
+    <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
+    <div class="contenedor">
+        <?php require "./partials/menu.php";?>
+        <main>
+            <?php 
+                $profile_pic="../img/default_user.png";
+                require "./partials/topbar.php";
+            ?>
+
+            <div class="detalleAnuncio">
+                <img src="https://via.placeholder.com/2500" alt="">
+
+                <div class="informacion">
+                    <h1>Nombre</h1>
+                    <hr>
+                    <h5 id="descripcion">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero exercitationem eligendi, ut quisquam laboriosam architecto illo natus, neque repudiandae sint qui eos deleniti. Est saepe quisquam delectus illo pariatur doloribus?</h5>
+                    <ion-icon id="favoritos" name="star-outline"></ion-icon>
+                    <hr>
+                    <p id="precio">16.99 &euro;</p>
+                </div>
+            </div>
+        </main>
+    </div>
+    <?php require "./partials/scripts.php";?>
+    
+</body>
+</html>
--- servidor/web/views/subir-anuncio.view.php
+++ servidor/web/views/subir-anuncio.view.php
@@ -16,7 +16,15 @@
                 require "./partials/topbar.php";
             ?>
 
-            <div class="contenedor-formulario">
+            <div class="contenido">
+                <div class="modal">
+                    <form action="" method="post">
+                        <h3>Convi&eacute;rtete en vendedor para desbloquear esta pagina</h3>
+                        <input type="text" name="direccion" id="direccion" autocomplete="off" required placeholder="Direcci&oacute;n">
+                        <input type="tel" name="telefono" id="telefono" autocomplete="off" required placeholder="Telefono">
+                        <input type="submit" name="registroVendedor" value="Confirmar">
+                    </form>
+                </div>
             <form action="" method="post">
                     <label for="img-input"><img src="#" alt="   " id="preview"><svg xmlns="http://www.w3.org/2000/svg" id="placeholder-img" viewBox="0 0 512 512"><title>Cloud Upload</title><path d="M320 367.79h76c55 0 100-29.21 100-83.6s-53-81.47-96-83.6c-8.89-85.06-71-136.8-144-136.8-69 0-113.44 45.79-128 91.2-60 5.7-112 43.88-112 106.4s54 106.4 120 106.4h56" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M320 255.79l-64-64-64 64M256 448.21V207.79"/></svg><span id="placeholder">Subir imagen</span></label>
                     <input type="file" name="imagen" id="img-input" accept="image/*">
@@ -66,7 +74,7 @@
                             <label for="cat6">Categor&iacute;a 5</label>
                         </div>
                     </div>
-                    <input type="submit" value="Publicar anuncio">
+                    <input type="submit" name="publicar" value="Publicar anuncio">
                 </form>
             </div>
         </main>
