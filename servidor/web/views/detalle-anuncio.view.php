787bb5bab4765050f8298cc6ee8c28a76bc43d28
Raul-Melgosa <raul.melgosa@ikasle.egibide.org>
2021-11-22 09:03:27 +0100

    detalles anuncios

--- servidor/web/views/detalle-anuncio.view.php
+++ servidor/web/views/detalle-anuncio.view.php
@@ -20,11 +20,11 @@
                 <img src="https://via.placeholder.com/2500" alt="">
 
                 <div class="informacion">
-                    <h1>Nombre</h1>
-                    <hr>
+                    <h1 id="nombre">Nombre</h1>
+                    <hr id="separador1">
                     <h5 id="descripcion">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero exercitationem eligendi, ut quisquam laboriosam architecto illo natus, neque repudiandae sint qui eos deleniti. Est saepe quisquam delectus illo pariatur doloribus?</h5>
                     <ion-icon id="favoritos" name="star-outline"></ion-icon>
-                    <hr>
+                    <hr id="separador2">
                     <p id="precio">16.99 &euro;</p>
                 </div>
             </div>
