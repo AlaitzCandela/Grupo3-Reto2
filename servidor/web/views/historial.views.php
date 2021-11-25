b46733d13ed0cc6c1aecec3e4cfaaa0a84eabf26
Raul-Melgosa <raul.melgosa@ikasle.egibide.org>
2021-11-25 09:18:40 +0100

    primer prototipo

---
+++ servidor/web/views/historial.views.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<?php
+    require "./partials/metas.php";
+?>
+    <title>Home</title>
+</head>
+<body>
+    <div class="contenedor">
+        <?php require "./partials/menu.php";?>
+        <main>
+            <?php 
+                $profile_pic="../img/default_user.png";
+                require "./partials/topbar.php";
+            ?>
+
+            <section>
+
+                <div titulo="HISTORIAL" class="pedidos">
+                    <div><span>Nombre del producto</span><span>Enlace</span><span>Fecha</span></div>
+                    <div class="anuncioLista"><span>Producto de prueba</span><span><a href="#">Ver el anuncio</a></span><span>dd/MM/yyyy</></div>
+                    <div class="anuncioLista"><span>Producto de prueba</span><span><a href="#">Ver el anuncio</a></span><span>dd/MM/yyyy</></div>
+                    <div class="anuncioLista"><span>Producto de prueba</span><span><a href="#">Ver el anuncio</a></span><span>dd/MM/yyyy</></div>
+                    <div class="anuncioLista"><span>Producto de prueba</span><span><a href="#">Ver el anuncio</a></span><span>dd/MM/yyyy</></div>
+                    <div class="anuncioLista"><span>Producto de prueba</span><span><a href="#">Ver el anuncio</a></span><span>dd/MM/yyyy</></div>
+                </div>
+
+            </section>
+        </main>
+    </div>
+    <?php require "./partials/scripts.php";?>
+</body>
+</html>
