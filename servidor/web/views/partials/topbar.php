26b9b90bf48451887f6d0110c96f5274b9bce1c7
Raul-Melgosa <raul.melgosa@ikasle.egibide.org>
2021-11-26 08:59:22 +0100

    filtros-favorito

--- servidor/web/views/partials/topbar.php
+++ servidor/web/views/partials/topbar.php
@@ -30,7 +30,7 @@
                 <div class="filtroButton">Filtro</div>
                 <div class="filtroButton">Filtro</div>
                 <div class="filtroButton">Filtro</div>
-                <div class="filtroButton" id="filtroFavoritos"></div>
+                <div class="filtroButton" id="filtroFavoritos">Favoritos</div>
                 <button class="accionFiltros" id="aplicarFiltros">Aplicar</button>
                 <button class="accionFiltros" id="resetearFiltros">Resetear</button>
             </div>
